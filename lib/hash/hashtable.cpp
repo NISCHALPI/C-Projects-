ff95dfa5b6d9c26b6843fc678ff317124fd2e5d0
HadesX <60031022+NISCHALPI@users.noreply.github.com>
2022-10-19 16:51:41 -0500

    Add files via upload

--- lib/hash/hashtable.cpp
+++ lib/hash/hashtable.cpp
@@ -1,10 +1,68 @@
+#include <iostream>
+#include "primes.h"
+#include "../list/list.h"
 
-// Implementaion Ideas
-// Quadratic Probing 
-// Resizable Hash
-// Good Hash Number 
-// Primes increament 
-// load factor and incrmentaion
-// Unform  Hash Function 
-// User Input Hash table 
-// Searh and O(n) Sorting  Implementation
+// Define rehashing capacity
+#ifndef __HASH_CAPACITY__
+#define __HASH_CAPACITY__ 50
+#endif
+
+
+// Uses Modulus Hash Function
+// HashTables Class
+template <typename Item>
+class HashTable{
+
+// Private vars 
+private:
+
+    // List of array  
+    List<Item>* __array = nullptr;
+     
+    // Size of Table 
+    int __size;
+
+
+    // Capacity 
+    int __capacity;
+
+    //HashFunction pointer 
+    int (*funcptr)(Item);
+
+    // currentLoad 
+    int __load;
+
+
+// Public Section 
+public:
+
+    // Constrcter 
+    HashTable(int (*funcptr)(Item)){
+
+        // Assign Hash Function Ptr to user define hash function
+        this->funcptr = funcptr;
+
+        // Assign the min array size 
+        __size = 0 ;
+
+        // Assigns Capcity to the environment variable
+        __capacity = __HASH_CAPACITY__;
+
+        // Intitialize Array
+        __array = new List<Item>[__capacity];
+
+    }
+
+
+
+    
+
+
+
+
+
+
+};
+
+
+
---
+++ lib/hash/primes.h
@@ -0,0 +1,65 @@
+#ifndef __PRIMES__LIB__
+#define __PRIMES__LIB__
+
+#include <iostream>
+#include <cmath>
+
+
+// O(sqrt(n)) algorithm 
+ bool __isPrime(int a){
+
+    // Guard
+    if (  a < 1){ throw std::logic_error("Primes are only defined for integers greater than 0");}
+
+    // If 1 or 2 
+    if ( a <= 2) {return true;}
+
+    // Factors are seen in pairs :-  a*b = n | a can run max upto sqrt(n)
+    // Checking for factors upto sqrt of n, implicitly checks the higher factors
+    for (int i = 2 ; i < int(sqrt(a) + 1) ; i++){
+        if ( a % i == 0){return false;}
+    }
+
+    //Return true if not divisible 
+    return true ;
+
+ }
+
+
+// Gets a higher prime than current number 
+int __getHigherPrime(int num){
+    
+    // temp val
+    int initialVal = num;
+
+    //increament first
+    num++ ;
+    
+    while (!__isPrime(num)){
+        num++;
+    }
+
+    return num;
+    
+}   
+
+int __getHigherPrime(int num, int minSpacing){
+    
+    // atleast must be greater than equal this
+    int atLeast = num + minSpacing;
+
+    // Loop
+    while (true){
+        
+        num = __getHigherPrime(num);
+        if(num > atLeast){
+            break;
+        }
+
+    }
+
+    return num;
+}
+
+
+#endif
