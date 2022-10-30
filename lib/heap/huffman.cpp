b1cebfcca816f8f33f2385fbbf9e2364bc2f20f3
Hades <nischalbhattaraipi@gmail.com>
2022-10-29 19:26:25 -0500

    Added Huffman

---
+++ lib/heap/huffman.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include "pqueue.h"
+#include "../hash/hashtable.h"
+#include <string>
+
+
+
+
+template <typename Item>
+using HashMap = chainedHash::HashTable<Item>;
+
+
+// Char to Int Func
+int __chartoint(char c){ return int(c);}
+
+
+
+
+
+
+
+
+
+template <typename Item>
+class Huffman{
+
+private:
+    
+    // Hash Map
+    HashMap<char> __freqMap;
+
+    // String To Process 
+    std::string __initSting;
+    
+    
+    
+
+
+
+};
+
+
+
